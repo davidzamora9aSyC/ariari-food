47f6c7830cf1a8b34d2b503d001d908e68550893
Tu Nombre <tuemail@dominio.com>
2024-11-28 22:23:49 -0500

    chimba

--- src/components/Propuesta.jsx
+++ src/components/Propuesta.jsx
@@ -2,7 +2,7 @@ const Propuesta = () => {
     return (
       <section id="propuesta" className="text-center py-20 bg-gradient-to-b from-white to-green-100">
         <h2 className="text-3xl font-bold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-green-800 to-orange-500">
-          ¿Por qué somos una chimba?
+          ¿Por qué elegir a Ariari?
         </h2>
         <p className="text-lg mb-8 px-4">
           Con Ariari, optimizamos la cadena de suministro para que el costo de tus frutas y verduras sea
